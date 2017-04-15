1f51c1980b38b3dae7236a8ddc06fd8b12867a8d
nguyenvantuan <tuannv92x@gmail.com>
2017-04-15 15:01:07 +0700

    Init Commit

---
+++ DemoUINavigationBartabBar/Singleton.swift
@@ -0,0 +1,14 @@
+//
+//  Singleton.swift
+//  DemoUINavigationBartabBar
+//
+//  Created by AnhTuan on 3/22/17.
+//  Copyright © 2017 AnhTuan. All rights reserved.
+//
+
+import Foundation
+class Singleton {
+    static let shared = Singleton()
+
+
+}
